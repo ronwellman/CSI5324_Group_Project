edc1c717446d056195582819e0ac907fd4b8b09f
Ron Wellman <ron.wellman01@gmail.com>
2023-02-14 00:19:29 -0500

    Rename user table and add additional constraints.

--- src/main/java/baylor/csi5324/group_project/Model/User.java
+++ src/main/java/baylor/csi5324/group_project/Model/User.java
@@ -2,7 +2,7 @@ package baylor.csi5324.group_project.Model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,23 +13,44 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = { "id", "email" }) })
-public class User implements Serializable {
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "id", "email" }) })
+public class User {
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
 
     private String street;
     private String city;
+    @Size(max = 2, message = "2 digit state")
     private String state;
     private String zip;
 
-    @Email
-    @NotNull
+    @Email(message = "valid email required")
+    @Column(name = "email", nullable = false)
     private String email;
     private String phone;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
