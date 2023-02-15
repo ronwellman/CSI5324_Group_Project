be9233b1f89911f3de984073b1ba6cf977861275
Ron Wellman <ron.wellman01@gmail.com>
2023-02-15 00:37:04 -0500

    Standardize naming of queries.

--- src/main/java/baylor/csi5324/group_project/Repository/UserRepository.java
+++ src/main/java/baylor/csi5324/group_project/Repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    public Optional<User> findUserById(Long id);
+    public Optional<User> findById(Long id);
 
-    public Optional<User> findUserByEmail(String email);
+    public Optional<User> findByEmail(String email);
 }
--- src/test/java/baylor/csi5324/group_project/Domain/UserTests.java
+++ src/test/java/baylor/csi5324/group_project/Domain/UserTests.java
@@ -1,18 +1,17 @@
 package baylor.csi5324.group_project.Domain;
 
 import baylor.csi5324.group_project.Repository.UserRepository;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserTests {
 
@@ -20,6 +19,7 @@ public class UserTests {
     private UserRepository userRepository;
 
     @Test
+    @Order(1)
     void createNewUser() {
         User user = new User();
         user.setFirstName("Albert");
@@ -34,7 +34,7 @@ public class UserTests {
         User saved = userRepository.save(user);
 
         Optional<User> userById = userRepository.findById(user.getId());
-        Optional<User> userByEmail = userRepository.findUserByEmail(user.getEmail());
+        Optional<User> userByEmail = userRepository.findByEmail(user.getEmail());
 
         assertTrue(userById.isPresent());
         assertEquals(saved, userById.get());
